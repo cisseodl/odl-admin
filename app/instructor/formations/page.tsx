cd8a8af807447f384e0ec7bdcd8cd8c5506ef883
Artifique <fombadaouda72@gmail.com>
2026-01-17 08:26:15 +0000

    fix: Ajout page instructor/formations

---
+++ app/instructor/formations/page.tsx
@@ -0,0 +1,5 @@
+import { InstructorFormationsList } from "@/components/instructor/formations/instructor-formations-list"
+
+export default function InstructorFormationsPage() {
+  return <InstructorFormationsList />
+}
