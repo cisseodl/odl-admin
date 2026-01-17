5bcf5fbe9a172a59c32182b90d83ce43309b8ab1
Artifique <fombadaouda72@gmail.com>
2026-01-17 17:21:20 +0000

    feat: Remplacer icon.svg par logo.png dans les mÃ©tadonnÃ©es du dashboard

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,16 +17,16 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
+        url: '/logo.png',
+        type: 'image/png',
+      },
+      {
         url: '/icon-light-32x32.png',
         media: '(prefers-color-scheme: light)',
       },
       {
         url: '/icon-dark-32x32.png',
         media: '(prefers-color-scheme: dark)',
-      },
-      {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
       },
     ],
     apple: '/apple-icon.png',
