1c7034303ae6ea5d999397e6c2c7c0e713266884
Artifique <fombadaouda72@gmail.com>
2026-02-09 17:07:35 +0000

    commit test

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { Geist, Geist_Mono } from 'next/font/google'
 import { AuthProvider } from '@/contexts/auth-context'
 import { LanguageProvider } from '@/contexts/language-context'
 import { ThemeProvider } from '@/components/theme-provider'
-import './globals.css'
+import './globals.css'   
 
 const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
